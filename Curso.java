010464eb0b51fbf70dcc15244fc10205b79088cc
Kelvin gomes pimentel rodrigo dos santos <106565458+kelvinGomesP@users.noreply.github.com>
2023-09-08 23:08:51 -0300

    Update Curso.java

--- Curso.java
+++ Curso.java
@@ -1,5 +1,40 @@
 package ex2;
 
-public class Curso {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Curso {
+    private String nome;
+    private List<Disciplina> disciplinas;
+    private List<Aluno> alunos;
+
+    public Curso(String nome) {
+        this.nome = nome;
+        this.disciplinas = new ArrayList<>();
+        this.alunos = new ArrayList<>();
+    }
+
+    public void adicionarDisciplina(Disciplina disciplina) {
+        disciplinas.add(disciplina);
+    }
+
+
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+
+   
+    public String getNome() {
+        return nome;
+    }
+
+   
+    public List<Disciplina> getDisciplinas() {
+        return disciplinas;
+    }
+
+   
+    public List<Aluno> getAlunos() {
+        return alunos;
+    }
 }
