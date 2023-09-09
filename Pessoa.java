a951e9273abd64a18e1a310c5c83e5a4cdb0b369
Kelvin gomes pimentel rodrigo dos santos <106565458+kelvinGomesP@users.noreply.github.com>
2023-09-08 23:06:33 -0300

    Atividade ||

---
+++ Pessoa.java
@@ -0,0 +1,18 @@
+package ex2;
+
+public class Pessoa {
+    private String nome;
+
+    
+    public Pessoa(String nome) {
+        this.nome = nome;
+       
+    }
+    
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+    
+  
+}
