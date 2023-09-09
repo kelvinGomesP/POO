a92d285979573eb07a30f1dbb3e9c92087705d5a
Kelvin gomes pimentel rodrigo dos santos <106565458+kelvinGomesP@users.noreply.github.com>
2023-09-08 23:10:45 -0300

    Update Turma.java

--- Turma.java
+++ Turma.java
@@ -1,5 +1,30 @@
 package ex2;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Turma {
+	private Professor professor;
+	private List<Aluno> alunos;
 
+public Turma(Professor professor) {
+	this.professor = professor;
+	this.alunos = new ArrayList<>();
+	
 }
+
+
+
+public void adicionarAluno(Aluno aluno) {
+    alunos.add(aluno);
+}
+
+public Professor getProfessor() {
+    return professor;
+}
+
+
+public List<Aluno> getAlunos() {
+    return alunos;
+}
+}
+	
