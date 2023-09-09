3ad870eef2be5b359a330221e231a223ae9e1898
Kelvin gomes pimentel rodrigo dos santos <106565458+kelvinGomesP@users.noreply.github.com>
2023-09-08 23:08:27 -0300

    Update Aluno.java

--- Aluno.java
+++ Aluno.java
@@ -1,5 +1,19 @@
 package ex2;
 
-public class Aluno {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Aluno extends Pessoa {
+    private Curso curso;
+
+    public Aluno(String nome,Curso curso) {
+        super(nome);
+        this.curso = curso;
+        curso.adicionarAluno(this);
+    }
+
+
+    public Curso getCurso() {
+        return curso;
+    }
 }
