a4b81de8c41973ecb33f3461ad4b7386e5c4b206
Kelvin gomes pimentel rodrigo dos santos <106565458+kelvinGomesP@users.noreply.github.com>
2023-09-08 23:10:17 -0300

    Update Professor.java

--- Professor.java
+++ Professor.java
@@ -1,5 +1,14 @@
 package ex2;
 
-public class Professor {
+public class Professor extends Pessoa {
+    private String disciplina;
 
+    public Professor(String nome, String disciplina) {
+        super(nome);
+        this.disciplina = disciplina;
+    }
+
+    public String getDisciplina() {
+        return disciplina;
+    }
 }
